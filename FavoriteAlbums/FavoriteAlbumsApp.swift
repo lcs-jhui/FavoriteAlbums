b6d29378bd7081f8e479d845fd4c95f9afb0e140
Justin Hui <jhui@lcs.on.ca>
2023-11-02 15:01:41 -0400

    Deleted content view and added a new group with a new file called views and albumview

--- FavoriteAlbums/FavoriteAlbumsApp.swift
+++ FavoriteAlbums/FavoriteAlbumsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FavoriteAlbumsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AlbumsView()
         }
     }
 }
---
+++ FavoriteAlbums/Views/AlbumsView.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumsView.swift
+//  FavoriteAlbums
+//
+//  Created by Justin Hui on 2/11/2023.
+//
+
+import SwiftUI
+
+struct AlbumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumsView()
+}
